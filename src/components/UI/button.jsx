d5ecba70f88ef9d9e2c68b8f01bb8a28226a6207
DianaCruz17 <catnip.diana@gmail.com>
2026-03-11 18:28:47 -0600

    :sparkles: book card and display all (#1)

---
+++ src/components/UI/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Button({ children }) {
+  return (
+    <button className='border rounded-3xl border-gray-100 gray px-5 py-2 bg-gray-100 text-xs font-semibold hover:bg-gray-300 cursor-pointer'>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
+//rfce
